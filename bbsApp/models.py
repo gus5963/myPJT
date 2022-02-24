0ec4abdf827c2fa6900c4ccf88b397088783cc8f
PHS <83255249+gus5963@users.noreply.github.com>
2022-02-24 11:14:51 +0900

    make bbs and comment table

--- bbsApp/models.py
+++ bbsApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class WebBbd(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    title = models.CharField(max_length=40)
+    writer = models.CharField(max_length=20)
+    # writer는 user랑 연동되어 login된 사람만 작성할 수 있도록 한다.
+    content = models.TextField()
+    regdate = models.DateField(auto_now=True)
+    viewcnt = models.IntegerField(default=0)
+
+
+class WebComment(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    txt = models.CharField(max_length=300)
+    writer = models.CharField(max_length=20)
+    # writer는 user랑 연동되어 login된 사람만 작성할 수 있도록 한다.
+    bbs_id = models.ForeignKey
+    regdate = models.DateField(auto_now=True)
+
+
+# 게시글 하나당 코멘트를 여러게 달 수 있도록 id를 코멘트에 FK로 달아준다.
